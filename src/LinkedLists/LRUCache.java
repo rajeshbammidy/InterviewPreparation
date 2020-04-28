0fa12f12ec176eda39240cc8a7707cdb0196fe57
Rajesh Bammidi <rajesh.bammidy@gmail.com>
2020-04-28 14:57:41 +0530

    New programmes

---
+++ src/LinkedLists/LRUCache.java
@@ -0,0 +1,140 @@
+package LinkedLists;
+
+import java.util.HashMap;
+
+/**
+ * Created by RajeshAatrayan|InterviewPreparation|LinkedLists|LRUCache| on Apr,2020
+ *  
+ * Happy Coding :)
+ **/
+public class LRUCache {
+    static class node {
+        node next;
+        node prev;
+        int val;
+
+        public node(int val) {
+            this.val = val;
+        }
+    }
+
+    int cap = 0;
+    int curCap = 0;
+    //To store the int - node values
+    HashMap<Integer, node> map;
+    //To store the key -value pairs
+    HashMap<Integer, Integer> valMap;
+    node head, tail;
+
+    /**
+     * Initialize them
+     *
+     * @param capacity
+     */
+    public LRUCache(int capacity) {
+        map = new HashMap<>();
+        valMap = new HashMap<>();
+        cap = capacity;
+        curCap = 0;
+        head = null;
+        tail = null;
+    }
+
+    /**
+     * Delete the least recently used page from the from and put in the back
+     */
+    void deleteAndPutBack(int key) {
+        node temp = map.get(key);
+        if (temp.prev != null) {
+            temp.prev.next = temp.next;
+        }
+        if (temp.next != null) {
+            temp.next.prev = temp.prev;
+        }
+        if (temp == head) {
+            head = head.next;
+        }
+        temp.next = null;
+
+        tail.next = temp;
+        temp.prev = tail;
+        tail = temp;
+    }
+
+    /**
+     * Return the key if it exists and delete from the front and put it in the back
+     * else return -1
+     */
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            node temp = map.get(key);
+            if (temp == tail || temp == tail && temp == head) return valMap.get(key);
+            deleteAndPutBack(key);
+            return valMap.get(key);
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+
+        /**
+         * It is the new value we are inserting
+         */
+        if (!map.containsKey(key)) {
+            //Its overflow
+            if (curCap == cap) {
+
+                map.remove(head.val);
+                valMap.remove(head.val);
+                head = head.next;
+                node newnode = new node(key);
+                newnode.prev = tail;
+                tail.next = newnode;
+                tail = newnode;
+                map.put(key, newnode);
+                valMap.put(key, value);
+
+
+            } else {
+                if (head == null && tail == null) {
+                    node newnode = new node(key);
+                    head = newnode;
+                    tail = newnode;
+                    map.put(key, newnode);
+                    valMap.put(key, value);
+                    curCap++;
+                    return;
+
+                }
+
+                node newnode = new node(key);
+                newnode.prev = tail;
+                tail.next = newnode;
+                tail = newnode;
+                map.put(key, newnode);
+                valMap.put(key, value);
+                curCap++;
+
+            }
+        }
+        /**
+         * If that key already present inside the map
+         */
+        else {
+            node temp = map.get(key);
+            if (temp == tail) {
+                valMap.put(key, value);
+                return;
+            }
+            deleteAndPutBack(key);
+            valMap.put(key, value);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/Test.java
+++ src/Test.java
@@ -17,53 +17,12 @@ import java.util.*;
  * Happy Coding :)
  */
 
-interface BinaryMatrix {
-    public int get(int row, int col);
+class LRUCache {
 
-    public List<Integer> dimensions
-
-            ();
-};
+}
 
 public class Test {
 
-    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
-        List<Integer> dim = binaryMatrix.dimensions();
-        int ans = dim.get(1) + 1;
-        int col = dim.get(1);
-        int row = dim.get(0);
-        int i = row - 1;
-        int j = col - 1;
-
-        while (i >= 0 && i < row && j >= 0 && j < col) {
-
-            int t = binarySearch(binaryMatrix, 1, i, j);
-            if (t != -1 && t < ans) {
-                ans = t;
-                j = t;
-            }
-            t--;
-
-        }
-        return ans;
-
-
-    }
-
-    private int binarySearch(BinaryMatrix binaryMatrix, int k, int row, int end) {
-        int beg = 0;
-        while (beg <= end) {
-            int mid = beg + (end - beg) / 2;
-            if (binaryMatrix.get(row, mid) == k && (mid - 1 < 0 || binaryMatrix.get(row, mid - 1) == 0)) {
-                return mid;
-            } else if (binaryMatrix.get(row, mid) < k) {
-                beg = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return -1;
-    }
 
     public static void main(String[] args) {
         System.out.println(Integer.toBinaryString(74));
