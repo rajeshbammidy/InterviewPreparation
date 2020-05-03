09c493f07c9e4b1d682ea6c29a165c3739710b08
Rajesh Bammidi <rajesh.bammidy@gmail.com>
2020-05-03 13:02:37 +0530

    New programmes

--- src/Test.java
+++ src/Test.java
@@ -17,23 +17,28 @@ import java.util.*;
  * Happy Coding :)
  */
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
 
 public class Test {
+    public int numJewelsInStones(String J, String S) {
 
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char ch : S.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        int sum = 0;
+        for (char ch : J.toCharArray()) {
+            if (map.get(ch) != null) {
+                sum += map.get(ch);
+            }
+        }
+        return sum;
+
+    }
 
     public static void main(String[] args) {
 //3*10 - 2%5/2sou
 
-        System.out.println(30-2%5/2);
+        System.out.println(30 - 2 % 5 / 2);
 
 
     }
