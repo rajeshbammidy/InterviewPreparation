37f10d7afae710a2598a84491ae078261c0d6b4e
Rajesh Bammidi <rajesh.bammidy@gmail.com>
2020-05-17 20:44:27 +0530

    Added new Programme

--- src/Test.java
+++ src/Test.java
@@ -22,84 +22,13 @@ import static BinarySearch.IsPerfectSquare.isPerfectSquare;
  */
 
 
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
 public class Test {
 
-    public ListNode oddEvenList(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode evenHead = null;
-        ListNode evenptr = null;
-        ListNode oddHead = null;
-        ListNode oddptr = null;
-        int c = 1;
-        while (head != null) {
-            if (c % 2 == 0) {
-                if (evenHead == null) {
-                    evenHead = head;
-                    evenptr = head;
-                    head = head.next;
-                    c++;
-                    continue;
-                }
-                evenptr.next = head;
-                evenptr = evenptr.next;
 
 
-            } else {
-
-                if (oddHead == null) {
-                    oddHead = head;
-                    oddptr = head;
-                    head = head.next;
-                    c++;
-                    continue;
-                }
-                oddptr.next = head;
-                oddptr = oddptr.next;
-
-
-            }
-            head = head.next;
-            c++;
-        }
-        evenptr.next=null;
-        if (evenHead != null) {
-            oddptr.next = evenHead;
-        }
-        return oddHead;
-
-
-    }
 
     public static void main(String[] args) {
-        ListNode node = new ListNode(1, null);
-        ListNode ptr = node;
-        for (int i = 2; i <= 3; i++) {
-            ptr.next = new ListNode(i, null);
-            ptr = ptr.next;
-        }
 
-        ptr = new Test().oddEvenList(node);
-        while (ptr != null) {
-            System.out.println(ptr.val);
-            ptr = ptr.next;
-        }
 
     }
 }
