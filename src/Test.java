89e14491b907322454fe8858f328e413d66e3996
Rajesh Bammidi <rajesh.bammidy@gmail.com>
2020-05-26 11:48:54 +0530

    added

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,7 @@ import com.sun.org.apache.regexp.internal.RE;
 import netscape.security.UserTarget;
 import org.omg.CORBA.INTERNAL;
 import org.omg.CORBA.MARSHAL;
+import sun.rmi.runtime.Log;
 
 
 import javax.print.attribute.HashAttributeSet;
@@ -25,45 +26,10 @@ import static BinarySearch.IsPerfectSquare.isPerfectSquare;
 
 
 public class Test {
-    static class node {
-        String ch;
-        int freq;
 
-        public node(char ch, int index) {
-            this.ch = ch+"";
-            this.freq = index;
-        }
-    }
 
-    public String frequencySort(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        ArrayList<node> list = new ArrayList<>();
-        int i = 0;
-        for (char ch : s.toCharArray()) {
-            if (map.containsKey(ch)) {
-                int ind = map.get(ch);
-                list.get(ind).freq += 1;
-                list.get(ind).ch= list.get(ind).ch+ch;
-
-            } else {
-                map.put(ch, i++);
-                list.add(new node(ch, 1));
-            }
-        }
-
-        Collections.sort(list, new Comparator<node>() {
-            @Override
-            public int compare(node o1, node o2) {
-                return -(o1.freq-o2.freq);
-            }
-        });
-        StringBuilder sb=new StringBuilder();
-        for(node x:list)sb.append(x.ch);
-        return sb.toString();
-    }
 
     public static void main(String[] args) {
-
-        System.out.println(new Test().frequencySort(""));
+        System.out.println(Math.log(16)/Math.log(2));
     }
 }
