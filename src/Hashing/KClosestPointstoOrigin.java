80ff019f8a5319fba923371611e2fc2108f5a037
Rajesh Bammidi <br185108@ncr.com>
2020-05-30 13:14:22 +0530

    Added

--- src/Hashing/KClosestPointstoOrigin.java
+++ src/Hashing/KClosestPointstoOrigin.java
@@ -1,32 +1,15 @@
+package Hashing;
 
-
-import com.sun.deploy.util.ArrayUtil;
-import com.sun.org.apache.regexp.internal.RE;
-import jdk.nashorn.internal.runtime.arrays.ArrayLikeIterator;
-import netscape.security.UserTarget;
-import org.omg.CORBA.INTERNAL;
-import org.omg.CORBA.MARSHAL;
-import sun.rmi.runtime.Log;
-
-
-import javax.print.attribute.HashAttributeSet;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.math.BigInteger;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-
-import static BinarySearch.IsPerfectSquare.isPerfectSquare;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
- * Created by RajeshAatrayan|InterviewPreparation|PACKAGE_NAME|null.java| on Oct,2019
+ * Created by RajeshAatrayan|InterviewPreparation|Hashing|KClosestPointstoOrigin| on May,2020
+ *  
  * Happy Coding :)
- */
-
-
-public class Test {
+ **/
+public class KClosestPointstoOrigin {
     static class node {
         int x;
         int y;
@@ -41,7 +24,7 @@ public class Test {
 
         TreeMap<Double, LinkedList<node>> map = new TreeMap<>();
         for (int i = 0; i < points.length; i++) {
-            double sq = Math.sqrt((Math.abs(points[i][0])*Math.abs(points[i][0]))+(Math.abs(points[i][1])*Math.abs(points[i][1]))) ;
+            double sq = Math.sqrt((Math.abs(points[i][0]) * Math.abs(points[i][0])) + (Math.abs(points[i][1]) * Math.abs(points[i][1])));
             if (map.get(sq) == null) {
                 LinkedList<node> list = new LinkedList<>();
                 list.add(new node(points[i][0], points[i][1]));
@@ -51,7 +34,7 @@ public class Test {
             }
         }
         int c = 0;
-        int ans[][] = new int[k + 1][2];
+        int ans[][] = new int[k][2];
         for (Map.Entry<Double, LinkedList<node>> x : map.entrySet()) {
 
             for (node l : x.getValue()) {
@@ -67,7 +50,4 @@ public class Test {
         return ans;
     }
 
-    public static void main(String[] args) {
-
-    }
 }
