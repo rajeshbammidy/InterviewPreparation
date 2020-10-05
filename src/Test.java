8ca1d5ceee267c21060e1bfbf9454c36c49ad027
Rajesh Bammidi <rajesh.bammidy@gmail.com>
2020-10-05 17:25:51 +0530

    added leetcode oct

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,21 @@ import java.util.*;
 
 
 public class Test {
+    public int bitwiseComplement(int n) {
+        String bis=Integer.toBinaryString(n);
+       String aux="";
+       for(char ch:bis.toCharArray()){
+           if(ch=='0'){
+               aux+="1";
+           }else{
+               aux+="0";
+           }
+       }
+        return Integer.valueOf(aux,2);
 
+    }
 
-
+    public static void main(String[] args) {
+        System.out.println(new Test().bitwiseComplement(0));
+    }
    }
