db54ffb6642205bcc21527256b10d29db34e0de6
rajeshbammidi <rajeshbammidi@urbancompany.com>
2022-06-13 19:26:45 +0530

    Added Questions for Math 2

--- src/ScalerAcademy/math_II/SizeThreeSubsequencesDivisibleByB.java
+++ src/ScalerAcademy/math_II/SizeThreeSubsequencesDivisibleByB.java
@@ -8,6 +8,39 @@ import java.util.ArrayList;
  * Date: May, 2022
  * Author: rajeshbammidi
  * Happy Coding :)
+ *
+ *
+ * Given an array of integers A and an integer B. Find and return the number of subsequences of length 3 whose sum is divisible by B.
+ *
+ * Since the total number of subsequences may be very large.
+ *
+ * Return the total number of subsequences of length 3 whose sum is divisible by B modulo (109+7).
+ *
+ *
+ * Input Format
+ *
+ * The only argument given is the integer array A.
+ * Output Format
+ *
+ * Return the number of subsequences of length 3 whose sum is divisible by B modulo (10^9+7).
+ * Constraints
+ *
+ * 1 <= length of the array <= 100000
+ * 1 <= A[i] <= 10^9
+ * 1 <= B <= 10^3
+ * For Example
+ *
+ * Input 1:
+ *     A = [6, 1, 1, 4, 1, 5, 3]
+ *     B = 2
+ * Output 1:
+ *     20
+ *
+ * Input 2:
+ *     A = [4, 10, 9]
+ *     B = 5
+ * Output 2:
+ *     0
  **/
 public class SizeThreeSubsequencesDivisibleByB {
     public int solve(ArrayList<Integer> list, int k) {
