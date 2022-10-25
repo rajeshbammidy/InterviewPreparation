1e3d71146bcf2055122d284ab9df43221ec0ae65
rajeshbammidi <rajes.bammidy@gmail.com>
2022-10-25 11:56:45 -0400

    Leetcode Day25

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import java.util.*;
 
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println(97-65);
+        int ascii = 'b';
+        System.out.println((char)(ascii-32) );
+    }
 
 
 }
