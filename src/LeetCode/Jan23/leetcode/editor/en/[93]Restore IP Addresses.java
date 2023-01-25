b62336f6875bf309f0170d7229c837e0195d2547
rajeshbammidi <rajes.bammidy@gmail.com>
2023-01-25 15:51:43 -0500

    Added Jan 25 lC

---
+++ src/LeetCode/Jan23/leetcode/editor/en/[93]Restore IP Addresses.java
@@ -0,0 +1,114 @@
+package LeetCode.Jan23.leetcode.editor.en;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A valid IP address consists of exactly four integers separated by single dots.
+ * Each integer is between 0 and 255 (inclusive) and cannot have leading zeros.
+ *
+ *
+ * For example, "0.1.2.201" and "192.168.1.1" are valid IP addresses, but "0.011.2
+ * 55.245", "192.168.1.312" and "192.168@1.1" are invalid IP addresses.
+ *
+ *
+ * Given a string s containing only digits, return all possible valid IP
+ * addresses that can be formed by inserting dots into s. You are not allowed to reorder or
+ * remove any digits in s. You may return the valid IP addresses in any order.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "25525511135"
+ * Output: ["255.255.11.135","255.255.111.35"]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "0000"
+ * Output: ["0.0.0.0"]
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: s = "101023"
+ * Output: ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= s.length <= 20
+ * s consists of digits only.
+ *
+ *
+ * Related Topics String Backtracking 👍 3482 👎 684
+ */
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution93 {
+    private List<String> result;
+
+    public List<String> restoreIpAddresses(String s) {
+        result = new ArrayList<>();
+        getIpAddresses(0, s.toCharArray(), new ArrayList<String>());
+        return result;
+    }
+
+    private void getIpAddresses(int index, char[] charArray, ArrayList<String> list) {
+        String tempIp = "";
+        /**
+         * If three fragments were already added, the below code snippet will help us to add the last fragment
+         * Ex: 192.168.121.___
+         */
+        if (list.size() == 3) {
+            int i;
+            /**
+             * Add the remaining digits to tempIp
+             */
+            for (i = index; i < index + 3 && i < charArray.length; i++) {
+                tempIp += charArray[i];
+            }
+            /**
+             * If i == charArray length and all the digits were added to fragment, add the final ip to result.
+             */
+            if (i == charArray.length && tempIp.length() > 0) {
+                if (!isValidIpFragment(tempIp)) return;
+                String ans = "";
+                for (String ip : list) ans += (ip + ".");
+                ans += tempIp;
+                result.add(ans);
+            }
+            return;
+        }
+
+        for (int i = index; i < index + 3 && i < charArray.length; i++) {
+            tempIp += charArray[i];
+            if (!isValidIpFragment(tempIp)) return;
+            list.add(tempIp);
+            getIpAddresses(i + 1, charArray, list);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    /**
+     * Check if the fragment is valid
+     */
+    boolean isValidIpFragment(String ipFrag) {
+        /**
+         * No fragment should have the start element as 0 if the len of the fragment is gte 2
+         */
+        if (ipFrag.length() > 1 && ipFrag.charAt(0) == '0') return false;
+        int number = Integer.parseInt(ipFrag);
+        /**
+         * The fragment should be between 0 - 255
+         */
+        if (number < 0 || number > 255) return false;
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/LeetCode/Jan23/leetcode/editor/en/[997]Find the Town Judge.java
@@ -0,0 +1,80 @@
+package LeetCode.Jan23.leetcode.editor.en;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * In a town, there are n people labeled from 1 to n. There is a rumor that one of
+ * these people is secretly the town judge.
+ *
+ * If the town judge exists, then:
+ *
+ *
+ * The town judge trusts nobody.
+ * Everybody (except for the town judge) trusts the town judge.
+ * There is exactly one person that satisfies properties 1 and 2.
+ *
+ *
+ * You are given an array trust where trust[i] = [ai, bi] representing that the
+ * person labeled ai trusts the person labeled bi.
+ *
+ * Return the label of the town judge if the town judge exists and can be
+ * identified, or return -1 otherwise.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: n = 2, trust = [[1,2]]
+ * Output: 2
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: n = 3, trust = [[1,3],[2,3]]
+ * Output: 3
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: n = 3, trust = [[1,3],[2,3],[3,1]]
+ * Output: -1
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= n <= 1000
+ * 0 <= trust.length <= 10⁴
+ * trust[i].length == 2
+ * All the pairs of trust are unique.
+ * ai != bi
+ * 1 <= ai, bi <= n
+ *
+ *
+ * Related Topics Array Hash Table Graph 👍 5045 👎 402
+ */
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution997 {
+    public int findJudge(int n, int[][] trust) {
+        if(trust.length==0 && n==1)return 1;
+        if(trust.length==0)return -1;
+        HashMap<Integer, Integer> keyMap = new HashMap();
+        HashMap<Integer, Integer> valueMap = new HashMap();
+        for (int i = 0; i < trust.length; i++) {
+            keyMap.put(trust[i][0], keyMap.getOrDefault(trust[i][0], 0) + 1);
+            valueMap.put(trust[i][1], valueMap.getOrDefault(trust[i][1], 0) + 1);
+        }
+        for (int i = 1; i <= n; i++) {
+            if (valueMap.containsKey(i) && (valueMap.get(i) == n - 1) && !keyMap.containsKey(i)) return i;
+        }
+        return -1;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
